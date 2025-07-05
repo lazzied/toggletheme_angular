e8a0f98af46b99f4b118089cb0902dd130baa6e3
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2025-07-05 15:25:56 +0100

    Added the necessary exports to the module

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import {
-    NbLayoutModule,NbCardModule,NbInputModule,NbButtonModule,NbThemeModule
-  } from '@nebular/theme';
+  NbLayoutModule, NbCardModule, NbInputModule, NbButtonModule, NbThemeModule
+} from '@nebular/theme';
 
-  @NgModule({
-    imports: [
-        NbCardModule,
-        NbInputModule,
-        NbButtonModule,
-        NbLayoutModule,
-        NbThemeModule
-    ]
-  })
+@NgModule({
+  imports: [
+    NbCardModule,
+    NbInputModule,
+    NbButtonModule,
+    NbLayoutModule,
+    NbThemeModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+  ],
+  exports: [
+    NbCardModule,
+    NbInputModule,
+    NbButtonModule,
+    NbLayoutModule,
+    NbThemeModule
+  ]
+})
 export class loginModule {
 
 }
